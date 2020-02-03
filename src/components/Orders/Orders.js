ded204e3bb6b5678996fe3b901bd979bc12d56d8
Andrea Benefiel <benefiel.andrea@gmail.com>
2020-02-02 22:27:58 -0700

    connecting Orders component to store

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Orders.css';
+import { connect } from 'react-redux';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
+class Orders extends Component {
+  render() {
+    const orderEls = this.props.orders.map(order => {
+      return (
+        <div className="order">
+          <h3>{order.name}</h3>
+          <ul className="ingredient-list">
+            {order.ingredients.map(ingredient => {
+              return <li>{ingredient}</li>
+            })}
+          </ul>
+        </div>
+      )
+    });
+
     return (
-      <div className="order">
-        <h3>{order.name}</h3>
-        <ul className="ingredient-list">
-          {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
-          })}
-        </ul>
-      </div>
+      <section>
+        {orderEls.length ? orderEls : <p>No orders yet!</p>}
+      </section>
     )
-  });
-
-  return (
-    <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
-    </section>
-  )
+  }
 }
 
-export default Orders;
+const mapStateToProps = state => ({
+  orders: state.orders
+});
+
+export default connect(mapStateToProps, null)(Orders);
